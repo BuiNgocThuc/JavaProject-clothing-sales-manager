424c0d656a217c7e6e136520756b63231170f1bd
Bui Ngoc Thuc <buingocthuc1512@gmail.com>
2023-03-04 20:27:45 +0700

    create project

---
+++ QuanLy_BanQuanAo/src/QuanLy_BanQuanAo_Object/NHACUNGCAP.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package QuanLy_BanQuanAo_Object;
+
+/**
+ *
+ * @author NGOC THUC
+ */
+public class NHACUNGCAP {
+    private String maNCC, tenNCC, diaChi, sdt;
+}
+
---
+++ QuanLy_BanQuanAo/src/QuanLy_BanQuanAo_Object/NHANVIEN.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package QuanLy_BanQuanAo_Object;
+
+/**
+ *
+ * @author NGOC THUC
+ */
+public class NHANVIEN {
+    private String maNV, tenNV, diaChi, sdt;
+    
+    
+}

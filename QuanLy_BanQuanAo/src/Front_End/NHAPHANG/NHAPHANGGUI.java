3c6351837fedf281c12605e99934e650dd30461b
Tran Nhat Qui <trannhatqui712@gmail.com>
2023-03-13 17:13:20 +0700

    Form NHAPHANG, BANHANG, SANPHAM

--- QuanLy_BanQuanAo/src/Front_End/NHAPHANG/NHAPHANGGUI.java
+++ QuanLy_BanQuanAo/src/Front_End/NHAPHANG/NHAPHANGGUI.java
@@ -4,10 +4,68 @@
  */
 package Front_End.NHAPHANG;
 
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import Front_End.SANPHAM.SANPHAMGUI;
 /**
  *
  * @author NGOC THUC
  */
 public class NHAPHANGGUI {
-    
+	JLabel labelMaSP, labelMaTH, labelTenSP, labelSize, labelColor, labelSoLuong, labelGia;
+	JTextField txtMaSP, txtMaTH, txtTenSP, txtSize, txtColor, txtSoLuong, txtGia;
+	
+    public NHAPHANGGUI(JFrame f) {
+		initComponent(f);
+    }
+		
+	public void initComponent(JFrame f)
+	{
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(7,2,5,5));
+		panel.setBounds(0,220 , 300, 200);
+		labelMaSP = new JLabel("Mã SP", JLabel.CENTER);
+		labelMaTH = new JLabel("Mã TH", JLabel.CENTER);
+		labelTenSP = new JLabel("Tên SP", JLabel.CENTER);
+		labelSize = new JLabel("Size", JLabel.CENTER);
+		labelColor = new JLabel("Màu sắc", JLabel.CENTER);
+		labelSoLuong = new JLabel("Số lượng", JLabel.CENTER);
+		labelGia = new JLabel("Đơn giá", JLabel.CENTER);
+		
+		txtMaSP = new JTextField(6);
+		txtMaTH = new JTextField(6);
+		txtTenSP = new JTextField(6);
+		txtSize = new JTextField(6);
+		txtColor = new JTextField(6);
+		txtSoLuong = new JTextField(6);
+		txtGia = new JTextField(6);
+		
+		panel.add(labelMaSP);
+		panel.add(txtMaSP);
+		panel.add(labelMaTH);
+		panel.add(txtMaTH);
+		panel.add(labelTenSP);
+		panel.add(txtTenSP);
+		panel.add(labelSize);
+		panel.add(txtSize);
+		panel.add(labelColor);
+		panel.add(txtColor);
+		panel.add(labelSoLuong);
+		panel.add(txtSoLuong);
+		panel.add(labelGia);
+		panel.add(txtGia);
+		
+		SANPHAMGUI a = new SANPHAMGUI();
+		a.createTbale(f);
+		
+		
+		f.add(panel);
+		
+	}
+		
 }
--- QuanLy_BanQuanAo/src/Front_End/SANPHAM/SANPHAMGUI.java
+++ QuanLy_BanQuanAo/src/Front_End/SANPHAM/SANPHAMGUI.java
@@ -4,10 +4,101 @@
  */
 package Front_End.SANPHAM;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.util.Vector;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import Back_End.SANPHAM.SANPHAM;
+
+
+
 /**
  *
  * @author NGOC THUC
  */
 public class SANPHAMGUI {
-    
+	private JTable tableSP;
+	private JLabel labelAdd, labelDelete, labelUpdate, labelFind;
+	static DefaultTableModel defaultTableModel;
+	private JScrollPane scrollPane ;
+	public  SANPHAMGUI(JFrame f)
+	{
+		initConponenrt(f);
+	}
+	public SANPHAMGUI()
+	{
+		
+	}
+	
+	public void createTbale(JFrame f) {
+		tableSP = new JTable();
+		defaultTableModel = new DefaultTableModel();
+		tableSP.setModel(defaultTableModel);
+		defaultTableModel.addColumn("Mã SP");
+		defaultTableModel.addColumn("Mã TH");
+		defaultTableModel.addColumn("Tên SP");
+		defaultTableModel.addColumn("Size");
+		defaultTableModel.addColumn("Màu sắc");
+		defaultTableModel.addColumn("Số lượng");
+		defaultTableModel.addColumn("Đơn giá");
+		defaultTableModel.addColumn("Trạng thái");
+		scrollPane = new JScrollPane(tableSP);
+		scrollPane.setBounds(0, 0, f.getWidth(), (f.getHeight())/2);
+		tableSP.setFillsViewportHeight(true);
+		f.add(scrollPane);
+	}
+
+	private void initConponenrt(JFrame f) {
+		
+		createTbale(f);
+		JPanel panelBottom = new JPanel();
+		labelAdd = new JLabel("THÊM", JLabel.CENTER);
+		labelAdd.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		labelAdd.setPreferredSize(new Dimension(40, 40));
+		
+		labelDelete = new JLabel("XÓA", JLabel.CENTER);
+		labelDelete.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		labelDelete.setPreferredSize(new Dimension(40, 40));
+		
+		labelUpdate = new JLabel("SỬA", JLabel.CENTER);
+		labelUpdate.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		labelUpdate.setPreferredSize(new Dimension(40, 40));
+		
+		labelFind = new JLabel("TÌM KIẾM", JLabel.CENTER);
+		labelFind.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		labelFind.setPreferredSize(new Dimension(60, 40));
+		
+		panelBottom.setBounds(0, scrollPane.getHeight() + f.getHeight()/4, f.getWidth(), 100);
+		panelBottom.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
+		panelBottom.add(labelAdd);
+		panelBottom.add(labelUpdate);
+		panelBottom.add(labelDelete);
+		panelBottom.add(labelFind);
+		f.add(panelBottom);
+		
+	}
+	
+	public void createValue(SANPHAM t) {
+		String maSP = t.getMaSP();
+		String maTH = t.getMaTH();
+		String tenSP = t.getTenSP();
+		String size = t.getKichCo();
+		String color = t.getMauSac();
+		int soLuong = t.getSoLuongSP();
+		float gia = t.getGiaSP();
+		String trangThai = t.getTrangThai();
+		Vector<SANPHAM> sanpham = new Vector<>();
+		sanpham.add(t);
+		defaultTableModel.addRow(new Object[] {maSP,maTH,tenSP,size,color,soLuong,gia, trangThai});
+	}
+
 }

c7b6ed96cd05ba413cfe9920eea472b1e0f50ec3
Bui Ngoc Thuc <107818282+BuiNgocThuc@users.noreply.github.com>
2023-05-09 20:04:29 +0700

    statistic

--- manage-clothing-store/src/Front_End/THONGKE/THONGKEGUI.java
+++ manage-clothing-store/src/Front_End/THONGKE/THONGKEGUI.java
@@ -35,14 +35,16 @@ import javax.swing.table.DefaultTableModel;
 import com.toedter.calendar.JDateChooser;
 
 import Back_End.THONGKE.ThongKeBUS;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  *
  * @author NGOC THUC
  */
 public class THONGKEGUI extends JPanel implements ChangeListener {
-	
-	public static JTable tblList;
+
+    public static JTable tblList;
     JTabbedPane tpStatistic = new JTabbedPane();
     JPanel pnKhachHang = new JPanel();
     JPanel pnNhaCungCap = new JPanel();
@@ -62,7 +64,7 @@ public class THONGKEGUI extends JPanel implements ChangeListener {
     JLabel lblProdData = new JLabel("0", JLabel.CENTER);
     JLabel lblCusData = new JLabel("0", JLabel.CENTER);
     JLabel lblProvData = new JLabel("0", JLabel.CENTER);
-    
+
     JPanel pnProdSta = new JPanel();
     JPanel pnCusSta = new JPanel();
     JPanel pnProvSta = new JPanel();
@@ -76,13 +78,13 @@ public class THONGKEGUI extends JPanel implements ChangeListener {
     JComboBox<String> cb_thongKe;
 //    JComboBox<String> cb_thongKe = new JComboBox<>();
     JFrame jf = new JFrame();
-    
+
     public THONGKEGUI() {
         ThongKeBUS a = new ThongKeBUS();
         initComponents();
         a.uploadSLB();
         a.applyDate(Date_Start, Date_End, lblApplyDate, lblReset, cb_thongKe);
-        
+
 //        jf.setSize(800, 500);
 //        jf.setLayout(new BorderLayout());
 //        jf.add(paneStatistic(), BorderLayout.CENTER);
@@ -98,7 +100,11 @@ public class THONGKEGUI extends JPanel implements ChangeListener {
 
     public JPanel panelTools(String[] items) {
         JPanel pnTool = new JPanel();
-        
+        JTextField txtTopSP = new JTextField();
+
+        txtTopSP.setBorder(BorderFactory.createTitledBorder("Top sản phẩm"));
+        txtTopSP.setPreferredSize(new Dimension(100, 40));
+
         JPanel pnDate = new JPanel();
         Date_Start = new JDateChooser();
         Date_End = new JDateChooser();
@@ -110,7 +116,22 @@ public class THONGKEGUI extends JPanel implements ChangeListener {
         cb_thongKe.setFont(new Font(null, Font.PLAIN, 12));
         cb_thongKe.setBackground(Color.WHITE);
         cb_thongKe.setOpaque(true);
-        
+        cb_thongKe.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        // Lấy giá trị được chọn và hiển thị lên JLabel
+                        String selectedItem = cb_thongKe.getSelectedItem().toString();
+                        switch(selectedItem) {
+                            case "Bán Chạy Nhất":
+                                
+                                break;
+                            case "Doanh Thu Cao Nhất":
+                            
+                            break;
+                        }
+                    }
+                });
+
         Date_Start.setPreferredSize(new Dimension(120, 45));
         Date_Start.setBackground(Color.WHITE);
         Date_Start.setOpaque(true);
@@ -127,7 +148,7 @@ public class THONGKEGUI extends JPanel implements ChangeListener {
         lblApplyDate.setBorder(BorderFactory.createLineBorder(Color.black));
         lblApplyDate.setIcon(new ImageIcon(getClass().getResource("/Icon/icon_img/icons8-done-32.png")));
         lblApplyDate.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        
+
         pnDate.setPreferredSize(new Dimension(420, 80));
         pnDate.setBackground(Color.white);
         pnDate.setOpaque(true);
@@ -137,16 +158,16 @@ public class THONGKEGUI extends JPanel implements ChangeListener {
         pnDate.add(Date_End);
         pnDate.add(lblApplyDate);
 
-
         lblReset.setPreferredSize(new Dimension(100, 40));
         lblReset.setBackground(Color.white);
         lblReset.setOpaque(true);
         lblReset.setBorder(BorderFactory.createLineBorder(Color.black));
         lblReset.setIcon(new ImageIcon(getClass().getResource("/Icon/icon_img/icons8-reset-32.png")));
         lblReset.setCursor(new Cursor(Cursor.HAND_CURSOR));
-        
-        pnTool.setPreferredSize(new Dimension(0, 150));
+
+        pnTool.setPreferredSize(new Dimension(0, 180));
         pnTool.setLayout(new FlowLayout(FlowLayout.CENTER, 40, 20));
+        pnTool.add(txtTopSP);
         pnTool.add(cb_thongKe);
         pnTool.add(pnDate);
         pnTool.add(lblReset);
@@ -214,25 +235,26 @@ public class THONGKEGUI extends JPanel implements ChangeListener {
         tblList = new JTable();
 
         DefaultTableModel model = new DefaultTableModel() {
-        	/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-			@Override
-        	public boolean isCellEditable(int row, int column) {
-        		return false;
-        	}
+            /**
+             *
+             */
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
         };
-        
-        for(String str : title) {
-			model.addColumn(str);
-		}
+
+        for (String str : title) {
+            model.addColumn(str);
+        }
 
         tblList.setModel(model);
 
         JScrollPane spnSta = new JScrollPane(tblList);
         spnSta.setBorder(BorderFactory.createLineBorder(Color.black));
-        
+
         return spnSta;
     }
 
@@ -247,7 +269,7 @@ public class THONGKEGUI extends JPanel implements ChangeListener {
     }
 
     public JPanel panelProduct() {
-        String[] itemsPro = new String[]{"Sắp Xếp Theo","Bán Chạy Nhất", "Doanh Thu Cao Nhất"};
+        String[] itemsPro = new String[]{"Sắp Xếp Theo", "Bán Chạy Nhất", "Doanh Thu Cao Nhất"};
         String[] title = new String[]{"Mã sản phẩm", "Tên sản phẩm", "Đơn giá", "Số lượng", "Tổng tiền"};
         pnSanPham.setPreferredSize(new Dimension(800, 500));
         pnSanPham.setLayout(new BorderLayout());
@@ -278,7 +300,7 @@ public class THONGKEGUI extends JPanel implements ChangeListener {
     }
 
     public JTabbedPane paneStatistic() {
-        tpStatistic.addTab("Thống Kê Tổng Quát", panelSummary());
+//        tpStatistic.addTab("Thống Kê Tổng Quát", panelSummary());
         tpStatistic.addTab("Sản Phẩm", panelProduct());
 //        tpStatistic.addTab("Khách Hàng", panelCustomer());
 //        tpStatistic.addTab("Nhà Cung Cấp", panelProvider());
